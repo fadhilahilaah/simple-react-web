994ecad0ab537c4e7887a2ebba43f1a51445c84c
hasbiafa <76551370+hasbiauzan19@users.noreply.github.com>
2024-07-28 02:48:54 +0700

    auto focus on email input when refresh the page

--- src/components/Fragments/FormLogin.jsx
+++ src/components/Fragments/FormLogin.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import Button from "../Elements/Button";
 import InputForm from "../Elements/Input";
 
@@ -6,8 +7,15 @@ const FormLogin = () => {
     event.preventDefault();
     localStorage.setItem("email", event.target.email.value);
     localStorage.setItem("password", event.target.password.value);
-    window.location.href = '/products';
+    window.location.href = "/products";
   };
+
+  const emailRef = useRef(null);
+
+  useEffect(() => {
+    emailRef.current.focus();
+  }, []);
+
   return (
     <form onSubmit={handleLogin}>
       {/* email */}
@@ -16,6 +24,7 @@ const FormLogin = () => {
         type="email"
         placeholder="example@gmail.com"
         name="email"
+        ref={emailRef}
       />
 
       {/* password */}
